dd73f7cbf9d80920d3d8b20c62119de147e66c8a
Rondinelle <rondinellewalitte@gmail.com>
2020-04-18 20:30:40 -0300

    excluido.dist.build

--- sistema/dist/utils/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});const headers = {
-  'content-type': 'application/x-www-form-urlencoded',
-  'Authorization': 'Token token="643136a91232953449d1ca8125594a25"',
-};
-
-exports. default = headers;
